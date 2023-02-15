ff6627a165dc2489d00e7ff0d9d3de1b410a00f5
Crevan <a.siverskii@gmail.com>
2023-02-15 21:42:08 +0300

    [fix] add checking for null; add methods for parsing LocalDate & LocalTime

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -9,14 +9,28 @@ public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+        return (startTime == null || lt.compareTo(startTime) >= 0) && (endTime == null || lt.compareTo(endTime) < 0);
     }
 
-    public static boolean isInside(LocalDate ldt, LocalDate startDateTime, LocalDate endTDateTime) {
-        return !ldt.isBefore(startDateTime) && !ldt.isAfter(endTDateTime);
+    public static boolean isInside(LocalDate ldt, LocalDate startDate, LocalDate endDate) {
+        return (startDate == null || !ldt.isBefore(startDate)) && (endDate == null || !ldt.isAfter(endDate));
     }
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+
+    public static LocalDate parseLocalDate(String param) {
+        if (param == null || param.isEmpty()) {
+            return null;
+        }
+        return LocalDate.parse(param);
+    }
+
+    public static LocalTime parseLocalTime(String param) {
+        if (param == null || param.isEmpty()) {
+            return null;
+        }
+        return LocalTime.parse(param);
     }
 }
